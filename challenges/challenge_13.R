2be9f6941c684fb178814aca266a5ac071786b7d
Dylan McDowell <dylan.mcdowell226@gmail.com>
2018-08-19 21:52:54 -0600

    changes

--- challenges/challenge_13.R
+++ challenges/challenge_13.R
@@ -1,4 +1,4 @@
-# Challenge 13 - "All your Base64"
+# Challenge 13 - "All your Base8^2"
 # ????
 
 # Q29uZ3JhdHVsYXRpb25zISBZb3UgaGF2ZSBzb2x2ZWQgQ2hhbGxlbmdlIDEzLiBZb3UgYXJlIG5vdyByZXdhcmRlZCAzMiBwb2ludHMu
