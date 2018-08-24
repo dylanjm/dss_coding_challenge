c4d35046eac199652b4011f69846c442c790e931
Cody Jorgensen <32916648+codemasta14@users.noreply.github.com>
2018-08-23 20:11:36 -0600

    added challenge_06.1.r

---
+++ challenge_06.1.r
@@ -0,0 +1,25 @@
+#Challenge_06.1 'Everlasting Love'
+
+# Your name is Julie, and you're a brand new freshman at BYU-Idaho
+# In the last three weeks you've given your number to 32 different boys, but only kissed 8 of them.
+# But no more of that because you have a boyfriend. His name is Cody and he's the most wonderful man in the world.
+# You've already had the marriage talk, and you're both positive that you're the most perfect match ever.
+# Lately though, you've been noticing how cute Kody's roommate, Dylan is. You've written your (soon to be) ex boyfriend pages and pages
+# Of love letters already and it would be a downright shame to let all that effort go to waste on someone that
+# Isn't as cute as Dylan. 
+
+# Challenge: Replace all mentions of Cody in the love letter with Dylan. Don't mess up or else you could be in 
+# A terribly awkward situation!
+
+#Bonus: That's not my name
+
+# Input Data (data/input_data_06.1.txt):
+
+#sample:
+#Cody, after being your best friend and telling you all my hopes and desires, 
+#I need to tell you what will make me happy.
+
+#Your code here:
+
+
+#Answer:
